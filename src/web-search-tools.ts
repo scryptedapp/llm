ab3e213c4c4452821ae7a6f5d6bac70837f6348c
Koushik Dutta <koushd@gmail.com>
2025-07-11 16:32:28 -0700

    typos

--- src/web-search-tools.ts
+++ src/web-search-tools.ts
@@ -90,7 +90,7 @@ export class WebSearchTools extends ScryptedDeviceBase implements LLMTools, Sett
                 return 'No results found.';
             }
 
-            const header = `The following are the Search results for "${query}". Consider using get-web-page-content tool in batches of 5 until you can arrive at an aswer the query. If a web page provides an answer to teh query, include the link in your response:\n`;
+            const header = `The following are the Search results for "${query}". Consider using get-web-page-content tool in batches of 5 until you can arrive at an answer the query. If a web page provides an answer to the query, include the link in your response:\n`;
 
             return header + data.results.map((result, index) =>
                 `
